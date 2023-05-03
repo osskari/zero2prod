df2b5e4b66b51c85c49f1aedc078887db5401eab
Óskar Jörgen Sandholt <oskarjorgen@gmail.com>
2023-05-03 21:26:58 +0000

    Chapter 5c: Dockerfile properly optimized and layered and cached

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -11,10 +12,11 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            // .await
-            .expect("Failed to connect to Postgres");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        // .await
+        .expect("Failed to connect to Postgres");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
