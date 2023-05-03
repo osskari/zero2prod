4e4eff89d213305816cb2f9be9a2ab18c8eac42e
Óskar Jörgen Sandholt <oskarjorgen@gmail.com>
2023-05-03 21:27:49 +0000

    Chapter 5c: clippy and fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
-use std::net::TcpListener;
 use sqlx::postgres::PgPoolOptions;
+use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
