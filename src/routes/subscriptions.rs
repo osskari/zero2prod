a4528f5e2705c44613b7b4b8cef0e8ce7ad34f10
Óskar Jörgen Sandholt <oskarjorgen@gmail.com>
2023-05-18 15:01:53 +0000

    Chapter 9

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -125,7 +125,7 @@ pub async fn send_confirmation_email(
     );
 
     email_client
-        .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
+        .send_email(&new_subscriber.email, "Welcome!", &html_body, &plain_body)
         .await
 }
 
@@ -165,7 +165,7 @@ fn generate_subscription_token() -> String {
         .collect()
 }
 
-fn error_chain_fmt(e: &impl std::error::Error, f: &mut Formatter<'_>) -> std::fmt::Result {
+pub fn error_chain_fmt(e: &impl std::error::Error, f: &mut Formatter<'_>) -> std::fmt::Result {
     writeln!(f, "{}\n", e)?;
     let mut current = e.source();
     while let Some(cause) = current {
